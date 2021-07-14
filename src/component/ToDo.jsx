5bab3f859d30043beb26eb8b0dcd3bb9bab26ed7
SAHASRABDI <sahasrabdi721306@gmail.com>
2021-07-15 00:02:51 +0530

    working commit

--- src/component/ToDo.jsx
+++ src/component/ToDo.jsx
@@ -4,15 +4,13 @@ function ToDo() {
   const [TaskArray, setTaskArray] = useState([]);
   const [Task, setTask] = useState("");
   const inputRef = useRef(null);
-
+  //so that the cursor stays at the text earea even after refresh
   useEffect(() => {
     inputRef.current.focus();
   });
 
   function Change(letters) {
     setTask(letters.target.value);
-
-    console.log(letters.target.value);
   }
   function Submit() {
     var tempTask = TaskArray;
@@ -20,23 +18,25 @@ function ToDo() {
       setTaskArray([...tempTask, Task]);
     }
     setTask("");
-    //console.log(tempTask);
   }
   function Delete(index) {
     // console.log(index.target.id);
     //var ind = index.target.id;
-    //var tempList = [...TaskArray];
-    var temp = 0;
-    var tempList = Array(TaskArray.length - 1);
-    // tempList.splice(ind, 1);
-    for (let i = 0; i < TaskArray.length; i += 1) {
-      if (i === index) {
-        continue;
-      } else {
-        tempList[temp] = TaskArray[i];
-        temp += 1;
-      }
-    }
+
+    // var temp = 0;
+    // var tempList = Array(TaskArray.length - 1);
+
+    // for (let i = 0; i < TaskArray.length; i += 1) {
+    //   if (i === index) {
+    //     continue;
+    //   } else {
+    //     tempList[temp] = TaskArray[i];
+    //     temp += 1;
+    //   }
+    // }
+    var tempList = [...TaskArray];
+    tempList.splice(index, 1);
+
     tempList = [...tempList];
     console.log(tempList);
     setTaskArray(tempList);
@@ -45,7 +45,7 @@ function ToDo() {
     <div>
       <div className="InputPart">
         <h1>TO DO LIST</h1>
-        {TaskArray.length == 0 ? (
+        {TaskArray.length === 0 ? (
           <div>
             <h2 style={{ color: "white" }}>You have no task to do</h2>
             <h2>WANT TO ADD A TASK??</h2>
